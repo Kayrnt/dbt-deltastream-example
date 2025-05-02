1a8dc56bf42c3edcc698623542729a94db9e81fc
Christophe Oudar <kayrnt@gmail.com>
2025-05-02 12:06:03 +0200

    fix postgres view

--- models/postgres_pageviews_zipcode_per_minute.sql
+++ models/postgres_pageviews_zipcode_per_minute.sql
@@ -11,8 +11,10 @@
   )
 }}
 SELECT 
-window_start, 
-window_end,
-zipcode, 
-pageviews
-FROM {{ ref('deltastream_pageviews_zipcode_per_minute') }}
+  window_start, 
+  window_end,
+  zipcode, 
+  COUNT(*) AS pageviews
+FROM HOP ({{ ref('csas_zipcode_pageviews') }}, SIZE 1 minute, ADVANCE BY 30 second)
+WITH ('timestamp'='viewtime')
+GROUP BY window_start, window_end, zipcode
